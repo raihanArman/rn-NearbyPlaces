a865fff1ddc7a5dcbc0e2e54d739ad81dfd4c428
raihanarman <raihanarman8@gmail.com>
2025-08-26 17:54:36 +0800

    Initialize React Native application architecture, incorporating empty directories for various modules such as assets and components, marked by `.gitkeep` files for git tracking, alongside a fundamental `app/index.tsx` file defining the primary application view with a centered 'Hello World' text element using React Native UI elements.

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+export default function App() {
+  return <View style={styles.container} >
+    <Text>Hello World</Text>
+  </View>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ffffff',
+  },
+});
